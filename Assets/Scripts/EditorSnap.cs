ee99d804375b396d4f7ce1f2915e4a32528a449a
gunnerr91 <blissvortex@hotmail.com>
2020-07-27 08:06:53 +0100

    added quad cube and snap script

---
+++ Assets/Scripts/EditorSnap.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour
+{
+    [Range(1f, 20f)] [SerializeField] float gridSize = 10f;
+
+    private void Update()
+    {
+        Vector3 snapPosition;
+        snapPosition.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPosition.y = Mathf.RoundToInt(transform.position.y / gridSize) * gridSize;
+        snapPosition.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        gameObject.transform.position = new Vector3(snapPosition.x, snapPosition.y, snapPosition.z);
+    }
+}
