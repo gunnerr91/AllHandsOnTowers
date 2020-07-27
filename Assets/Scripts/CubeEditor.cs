6f3b9f0519b2a467d476822429a6e979c4ed21c1
gunnerr91 <blissvortex@hotmail.com>
2020-07-27 09:04:25 +0100

    update cube script to display cube's co-ordinates

--- Assets/Scripts/CubeEditor.cs
+++ Assets/Scripts/CubeEditor.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour
+public class CubeEditor : MonoBehaviour
 {
     [Range(1f, 20f)] [SerializeField] float gridSize = 10f;
+
+    TextMesh labelTextMesh;
 
     private void Update()
     {
         Vector3 snapPosition;
+
         snapPosition.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
-        snapPosition.y = Mathf.RoundToInt(transform.position.y / gridSize) * gridSize;
         snapPosition.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
 
-        gameObject.transform.position = new Vector3(snapPosition.x, snapPosition.y, snapPosition.z);
+        gameObject.transform.position = new Vector3(snapPosition.x, 0, snapPosition.z);
+        
+        labelTextMesh = gameObject.GetComponentInChildren<TextMesh>();
+        labelTextMesh.text = snapPosition.x / gridSize + "," + snapPosition.z / gridSize;
     }
 }
