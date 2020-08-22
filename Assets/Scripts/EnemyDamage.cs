1b0398efc59183ca782059f3d401d903eed155b5
gunnerr91 <blissvortex@hotmail.com>
2020-08-22 09:45:02 +0100

    added particle effect for enemy damage

--- Assets/Scripts/EnemyDamage.cs
+++ Assets/Scripts/EnemyDamage.cs
@@ -3,28 +3,33 @@
 public class EnemyDamage : MonoBehaviour
 {
     [SerializeField] int EnemyHP = 20;
+    [SerializeField] ParticleSystem hitParticlePrefab;
+    [SerializeField] ParticleSystem deathParticlePrefab;
 
     void Start()
     {
-        
+
     }
 
     private void OnParticleCollision(GameObject other)
     {
         UpdateEnemyHp();
-        if(EnemyHP < 1)
+        if (EnemyHP < 1)
         {
             KillEnemy();
         }
     }
-    
+
     private void UpdateEnemyHp()
     {
         EnemyHP -= 1;
+        //hitParticlePrefab.Play();
     }
 
     private void KillEnemy()
     {
+        var vfx = Instantiate(deathParticlePrefab, transform.position, Quaternion.identity);
+        vfx.Play();
         Destroy(gameObject);
     }
 
