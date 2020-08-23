7b77c818f065d3bc8670e9c1a03e832a2421ecaa
gunnerr91 <blissvortex@hotmail.com>
2020-08-23 09:55:01 +0100

    added score and player hp text in game scene

--- Assets/Scripts/MainPlayerBase.cs
+++ Assets/Scripts/MainPlayerBase.cs
@@ -1,15 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainPlayerBase : MonoBehaviour
 {
-    [SerializeField] float playerBaseHP = 10;
-    [SerializeField] int healthDecrease = -1;
+    [SerializeField] int playerBaseHP = 10;
+    [SerializeField] int healthDecrease = 1;
+
+    [SerializeField] Text playerBaseHPText;
+
+    private void Start()
+    {
+        UpdatePlayerBaseHPText();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         playerBaseHP -= healthDecrease;
+        print(playerBaseHP);
+        UpdatePlayerBaseHPText();
     }
+
+    private void UpdatePlayerBaseHPText()
+    {
+        playerBaseHPText.text = "Player base HP " + playerBaseHP;
+    }
+
 }
 
--- Assets/Scripts/SpawnEnemies.cs
+++ Assets/Scripts/SpawnEnemies.cs
@@ -1,15 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpawnEnemies : MonoBehaviour
 {
     [Range(0.1f, 120f)] [SerializeField] float secondsBetweenSpawns = 2f;
     [SerializeField] Enemy enemyObject;
     [SerializeField] Transform enemyParentTransform;
+    [SerializeField] int gameScore = 0;
+    [SerializeField] Text gameScoreText;
+
+
     void Start()
     {
         StartCoroutine(SpawnEnemy());
+
+    }
+
+    private void Update()
+    {
+        gameScoreText.text = "Score: " + gameScore;
     }
 
     IEnumerator SpawnEnemy()
@@ -17,6 +28,7 @@ public class SpawnEnemies : MonoBehaviour
         while (true)
         {
             var newEnemy = Instantiate(enemyObject, transform.position, Quaternion.identity);
+            gameScore++;
             newEnemy.transform.parent = enemyParentTransform.transform;
             yield return new WaitForSeconds(secondsBetweenSpawns);
         }
